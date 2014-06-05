bb3346d45d93069547906c7ba3f501e8582dbd3c
Héctor Gómez <hectorgomeziglesias@yahoo.es>
2014-06-05 10:55:56 +0200

    First functionality for the Flexboard object

---
+++ controller.js
@@ -0,0 +1,35 @@
+var Flexboard = (function () {
+
+    var container;
+
+    /**
+     * Give value to all variables and initialize event listeners
+     */
+    function init() {
+        container = document.getElementById("container");
+    }
+
+    /**
+     * Add a new element to the board
+     *
+     * @param {object} item Element to be added
+     */
+    function addItem(item) {
+        var iframe = document.createElement("iframe");
+
+        // Populate the values
+        iframe.src = item.url;
+        iframe.className = "item";
+
+        // Add the new element
+        container.appendChild(iframe);
+    }
+
+    // Initialize the application once the body is ready
+    window.onload = init;
+
+    // Exposed API
+    return {
+        addItem: addItem
+    };
+})();
