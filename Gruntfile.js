909e10288b69336e5fbda0e83b47bcc3f737036d
Héctor Gómez <hectorgomeziglesias@yahoo.es>
2014-06-19 22:53:30 +0200

    Introduced Grunt and Browserify

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+/* jshint es3:false */
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.initConfig({
+        browserify: {
+            dist: {
+                files: {
+                    'flexboard.js': 'src/flexboard.js'
+                }
+            }
+        }
+    });
+
+};
